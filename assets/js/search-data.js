a5cecc5e410c130066ba2bf1ea132778e7047713
njcorrente <49278064+njcorrente@users.noreply.github.com>
2025-02-21 23:00:22 +0000

    Deploying to gh-pages from @ njcorrente/njcorrente.github.io@7860299a6b7e0b8190fde669812698149cfdb281 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,14 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6E%69%63%68%6F%6C%61%73.%63%6F%72%72%65%6E%74%65@%72%75%74%67%65%72%73.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/njcorrente", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/njcorrente", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-5765-1806", "_blank");
         },
       },{
         id: 'social-rss',
@@ -513,14 +527,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=G7dbblQAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/njcorrente", "_blank");
         },
       },{
       id: 'light-theme',
