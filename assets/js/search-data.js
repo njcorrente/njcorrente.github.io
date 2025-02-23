e6a017a352aa9db86ffd82f251fb4096e4dc342a
njcorrente <49278064+njcorrente@users.noreply.github.com>
2025-02-23 00:54:17 +0000

    Deploying to gh-pages from @ njcorrente/njcorrente.github.io@f5870d1bbcd173a4df83911c64e85418f9267285 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,13 +105,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0001-5765-1806", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
